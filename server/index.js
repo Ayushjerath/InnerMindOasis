d3d567693b45349e94eaf4c469a41afb6c8d76a2
Ayushjerath <106790961+Ayushjerath@users.noreply.github.com>
2024-04-01 03:19:02 +0530

    Frontend Setup and API Creation
    Created controller and their routed for different function.
    
    Created APIs for the frontend.
    
    Setup the react frontend project.

--- server/index.js
+++ server/index.js
@@ -4,12 +4,20 @@ const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectDb = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+const postRoutes = require("./routes/postRoutes");
+const miscRoutes = require("./routes/miscRoutes");
 const port = process.env.PORT || 3001;
 
 connectDb();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("tiny"));
+app.use("/auth", authRoutes);
+app.use("/comment", commentRoutes);
+app.use("/post", postRoutes);
+app.use("/misc", miscRoutes);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
